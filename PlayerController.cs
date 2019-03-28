34539a756591dc2add8fc1b4e587b25baef82e90
26583 <43673592+26583@users.noreply.github.com>
2019-03-28 14:12:14 +0100

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    public float moveInput;
+    SpriteRenderer sr;
+    private Rigidbody2D rb;
+
+    private int extraJump;
+    public int extraJumpValue;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+
+    public Transform groundCheck;
+
+    public float checkRadius;
+
+    public LayerMask whatIsGround;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        extraJump = extraJumpValue;
+    }
+
+    void Update()
+    {
+        if (isGrounded) {
+            extraJump = extraJumpValue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)&& extraJump > 0){
+            rb.velocity = Vector2.up * jumpForce;
+            extraJump--;
+        }/*else if(Input.GetKeyDown(KeyCode.Space) && extraJump == 0) {
+            rb.velocity = Vector2.up * jumpForce;
+        }*/
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        if (facingRight == false && moveInput >0) {
+            Flip();
+        } else if(facingRight == true && moveInput < 0) {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        sr.flipX = !facingRight;
+    }
+}
